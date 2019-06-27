da445053e9ece6f8e4810ae1215e5581482978d6
christliu <215688426@qq.com>
2019-06-27 16:59:06 +0800

    add 2 examples of geometry shader

---
+++ src/9.2.ExplodeShader/main.cpp
@@ -0,0 +1,164 @@
+#include <iostream>
+
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// GLFW
+#include <GLFW/glfw3.h>
+#include <soil.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include"Shader.h"
+#include"Camera.h"
+#include "Model.h"
+#include "Texture.h"
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+
+const GLuint WIDTH = 800;
+const GLuint HEIGHT = 600;
+
+glm::vec3 cameraPos = glm::vec3(0, 0, 3);
+
+//GLfloat cameraSpeed = 5.0f;
+GLboolean keys[1024];
+GLfloat delta = 0.0f;
+GLfloat lastframe = GLfloat(glfwGetTime());
+
+GLfloat lastX = 400.0f, lastY = 300.0f;
+GLfloat firstMouse = true;
+
+Camera camera(cameraPos);
+
+void doMovement();
+
+// The MAIN function, from here we start the application and run the game loop
+int main()
+{
+	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
+	// Init GLFW
+	glfwInit();
+	// Set all the required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+
+	// Create a GLFWwindow object that we can use for GLFW's functions
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	glfwMakeContextCurrent(window);
+
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
+	glewExperimental = GL_TRUE;
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewInit();
+	// Define the viewport dimensions
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	glViewport(0, 0, width, height);
+
+	Model ourModel("res/nanosuit/nanosuit.obj");
+	Shader modelShader("shader/9.2.Explode.vs", "shader/model.ps", "shader/9.2.Explode.gs");
+	
+	glEnable(GL_DEPTH_TEST);
+
+	GLfloat start = (GLfloat)glfwGetTime();
+	// Game loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+		// Render
+		// Clear the colorbuffer
+		glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		delta = (GLfloat)glfwGetTime() - lastframe;
+		lastframe = (GLfloat)glfwGetTime();
+
+		doMovement();
+		glm::mat4 model = glm::mat4(1.0f);
+
+		glm::mat4 view = camera.GetViewMatrix();
+
+		glm::mat4 perspective = glm::mat4(1.0f);
+		perspective = glm::perspective(glm::radians(camera.Zoom), float(WIDTH) / float(HEIGHT), 0.1f, 100.0f);
+
+
+		glBindVertexArray(0);
+
+		modelShader.Use();
+		model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // translate it down so it's at the center of the scene
+		model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));
+		glUniformMatrix4fv(glGetUniformLocation(modelShader.m_shaderProgram, "model"), 1, GL_FALSE, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(modelShader.m_shaderProgram, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(modelShader.m_shaderProgram, "projection"), 1, GL_FALSE, glm::value_ptr(perspective));
+		glUniform1f(glGetUniformLocation(modelShader.m_shaderProgram, "time"), lastframe);
+
+		ourModel.Draw(modelShader);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+	// Properly de-allocate all resources once they've outlived their purpose
+	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glfwTerminate();
+	return 0;
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = xpos, lastY = ypos;
+		firstMouse = false;
+	}
+	GLfloat deltax = xpos - lastX;
+	GLfloat dletay = ypos - lastY;
+	camera.ProcessMouseMovement(deltax, dletay);
+	lastX = xpos;
+	lastY = ypos;
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.ProcessMouseScroll(yoffset);
+}
+
+void doMovement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W])
+		camera.ProcessKeyboard(FORWARD, delta);
+	if (keys[GLFW_KEY_S])
+		camera.ProcessKeyboard(BACKWARD, delta);
+	if (keys[GLFW_KEY_A])
+		camera.ProcessKeyboard(LEFT, delta);
+	if (keys[GLFW_KEY_D])
+		camera.ProcessKeyboard(RIGHT, delta);
+}
